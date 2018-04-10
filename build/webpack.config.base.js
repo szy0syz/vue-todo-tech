472f217d40432e15bd513b14ad6d57a6be60cf7a
Jerry Shi <szy0syz@gmail.com>
2018-04-10 14:36:09 +0800

    update 2-1 升级改造wepback Part2 重建目录结构

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -2,10 +2,10 @@ const path = require('path')
 
 const config = {
   target: 'web',
-  entry: path.join(__dirname, '../src/index.js'),
+  entry: path.join(__dirname, '../client/index.js'),
   output: {
     filename: 'bundle.[hash:8].js',
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, '../dist')
   },
   module: {
     rules: [
