43ac62737a80c081a7d7fdda751879964b79be43
szy0syz <szy0syz@gmail.com>
2018-04-16 19:31:14 +0800

    update 2.3 css module配置

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -4,7 +4,10 @@ module.exports = isDev => {
   return {
     preserveWhitespace: false,    // 如果设置为 false，模版中 HTML 标签之间的空格将会被忽略
     extractCSS: !isDev,           // 排除vue文件中css单独提打包到统一css文件，使用vue异步加载是不需要所有样式都去统一css文件加载，这样可以提高首屏加载速度
-    cssModules: {},
+    cssModules: {
+      localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
+      camelCase: true
+    },
     // hotReload: true,          // 根据环境变量生成
     // loaders: {
     //   'docs': docsLoader
