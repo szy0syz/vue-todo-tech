1e0f2421bd4b5ae0b56c9fa363266647062e0ce9
szy0syz <szy0syz@gmail.com>
2018-04-16 19:10:39 +0800

    update 2.2 vue-loader配置 Part2

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,6 +1,17 @@
+// options中文：https://github.com/vuejs/vue-loader/blob/master/docs/zh-cn/options.md
+
 module.exports = isDev => {
   return {
     preserveWhitespace: false,    // 如果设置为 false，模版中 HTML 标签之间的空格将会被忽略
-    extractCSS: isDev,            // 排除vue文件中css单独提打包到统一css文件，使用vue异步加载是不需要所有样式都去统一css文件加载，这样可以提高首屏加载速度
+    extractCSS: !isDev,           // 排除vue文件中css单独提打包到统一css文件，使用vue异步加载是不需要所有样式都去统一css文件加载，这样可以提高首屏加载速度
+    cssModules: {},
+    // hotReload: true,          // 根据环境变量生成
+    // loaders: {
+    //   'docs': docsLoader
+    // },
+    // preLoader: {
+    //   js: 'typescript-loader' // 预处理loader
+    // },
+    // postLoader: {}
   }
 }
